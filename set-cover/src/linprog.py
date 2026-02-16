2deb415b3a4f5439bb04da670fd1030cd43254b9
Gleb Kostylev <yaglebkostylev2005@gmail.com>
2026-02-16 12:09:19 +0300

    good one

--- set-cover/src/linprog.py
+++ set-cover/src/linprog.py
@@ -21,30 +21,45 @@ at_least = np.ones(shape=item_count)
 match_matrix = np.array(match_matrix)
 weights = np.array(weights)
 
-solution = scipy.optimize.linprog(c=weights, A_ub=-match_matrix, b_ub=-at_least, bounds=(0, 1)).x
+lin_solution = scipy.optimize.linprog(c=weights, A_ub=-match_matrix, b_ub=-at_least, bounds=(0, 1)).x
+approx_answer_size = np.sum(lin_solution)
+approx_complexity = set_count * item_count * approx_answer_size
 
-print(solution)
+best_score = 1e100
+best_answer = []
 
-print(24, solution @ weights, file=sys.stderr)
+print(approx_complexity, file=sys.stderr)
 
-la = 24 / solution @ weights
-best_answer = np.ones(shape=set_count)
-best_score = weights @ best_answer
-while True:
-    chance = solution * la
-    rnd = np.random.random(size=set_count)
-    taken = rnd < chance
-    score = weights @ taken
-    if score > 24 + 1e-6:
-        continue
-    coverage = match_matrix @ taken
-    if not np.all(coverage >= 1 - 1e-6):
-        continue
-    best_score = score
-    best_answer = taken
-    break
+for i in range(10):
+    cur_solution = np.zeros(set_count)
 
-print(f"Score: {best_score}")
+    conditions = []
+    new_at_least = []
+
+    while True:
+        coverage = match_matrix @ cur_solution
+        if np.min(coverage) > 1 - 1e-6:
+            break
+        
+        A_ub = -np.concatenate((match_matrix, np.array(conditions))) if len(conditions) > 0 else -match_matrix
+        b_ub = -np.concatenate((at_least, np.array(new_at_least))) if len(new_at_least) > 0 else -at_least
+
+        noise = (np.random.random(set_count) * 2 - 1) * 1e-1
+        solution = scipy.optimize.linprog(c=weights + noise, A_ub=A_ub, b_ub=b_ub, bounds=(0, 1)).x
+        diff = solution - cur_solution
+        mx = np.argmax(diff)
+        cur_solution[mx] = 1
+
+        conditions.append(np.zeros(set_count))
+        conditions[-1][mx] = 1
+        new_at_least.append(1)
+
+    score = weights @ cur_solution
+    if score < best_score:
+        best_score = score
+        best_answer = cur_solution
+
+print(f"Score: {int(round(best_score))}")
 for i in range(set_count):
     if best_answer[i] >= 1 - 1e-6:
         print(*datas[i])
--- set-cover/src/main.cpp
+++ set-cover/src/main.cpp
@@ -14,8 +14,8 @@ void beam(const SetCover& task, size_t bound) {
     beam.print();
 }
 
-void call_python() {
-    freopen("./data/_scored_sc_330_0", "r", stdin);
+void call_python(char* input) {
+    freopen(input, "r", stdin);
     std::string python = "./venv/bin/python3";
     std::string file = "./src/linprog.py";
     char* argv[3];
@@ -25,19 +25,20 @@ void call_python() {
     execve(python.data(), argv, nullptr);
 }
 
-int main() {
+int main(int argc, char** argv) {
     SetCover task = SetCover::read();
     if (task.number_of_items() <= 30) {
         dynamic_programming(task);
         return 0;
     }
-    if (task.number_of_sets() < 500) {
-        call_python();
-        return 0;
-    }
-    if (task.number_of_sets() <= 1000) {
-        beam(task, 10);
-    } else {
-        greedy(task);
-    }
+    call_python(argv[1]);
+    // if (task.number_of_sets() < 500) {
+    //     call_python(argv[1]);
+    //     return 0;
+    // }
+    // if (task.number_of_sets() <= 1000) {
+    //     beam(task, 10);
+    // } else {
+    //     greedy(task);
+    // }
 }
